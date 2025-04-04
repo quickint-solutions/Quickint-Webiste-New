5ec558781b315b679b7cc6f58d7d28efd06f4fca
sagar koshti <koshtisagar7778@gmail.com>
2025-04-04 17:45:07 +0530

    logo size increase and swiper add

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -3,21 +3,23 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
+import { Navigation, Autoplay } from "swiper/modules";
+
 
 // Install Swiper modules
 
 const BrandArea = () => {
     const brandImages = [
-        "assets/images/brand/sonylogo.png",
-        "assets/images/brand/publiclogo.png",
         "assets/images/brand/1deallogo.png",
         "assets/images/brand/carelogo.png",
-        "assets/images/brand/willwarelogo.png",
-        "assets/images/brand/greendoorslogo.png",
         "assets/images/brand/cattlefeed.png",
         "assets/images/brand/edgeinfinium.png",
+        "assets/images/brand/greendoorslogo.png",
         "assets/images/brand/hitechlogo.png",
         "assets/images/brand/maanlogo.png",
+        "assets/images/brand/publiclogo.png",
+        "assets/images/brand/sonylogo.png",
+        "assets/images/brand/willwarelogo.png",
     ];
 
     return (
@@ -25,21 +27,40 @@ const BrandArea = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
-                        <div className="brand-area-main-wrapper">
+                        <div className="brand-area-main-wrapper swiper-area-main-wrapper">
                             <Swiper
-                                spaceBetween={20}
-                                slidesPerView={5}
+                                spaceBetween={0}
+                                slidesPerView={4}
                                 loop={true}
-                                breakpoints={{
-                                    320: { slidesPerView: 2 },
-                                    768: { slidesPerView: 3 },
-                                    1024: { slidesPerView: 5 },
+                                autoplay={{ delay: 3000, disableOnInteraction: false }}
+                                navigation={{
+                                    nextEl: ".swiper-button-next",
+                                    prevEl: ".swiper-button-prev",
                                 }}
+                                breakpoints={{
+                                    // When the viewport is >= 640px
+                                    240: {
+                                        slidesPerView: 1,
+                                        spaceBetween: 0,
+                                    },
+                                    // When the viewport is >= 768px
+                                    768: {
+                                        slidesPerView: 1,
+                                        spaceBetween: 0,
+                                    },
+                                    // When the viewport is >= 1024px
+                                    1024: {
+                                        slidesPerView: 2,
+                                        spaceBetween: 0,
+                                    },
+                                }}
+                                className="mySwiper-testimonials-150"
+                                modules={[Navigation, Autoplay]}
                             >
                                 {brandImages.map((image, index) => (
                                     <SwiperSlide key={index}>
                                         <div className="single-brand">
-                                            <img className="brand-image" src={image} alt={`brand-${index + 1}`} />
+                                            <img src={image} alt={`brand-${index + 1}`} />
                                         </div>
                                     </SwiperSlide>
                                 ))}
