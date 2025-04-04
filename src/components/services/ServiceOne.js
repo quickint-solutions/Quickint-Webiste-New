59696c5ffbac6b8751c8053861225f23df0ffc8c
sagar koshti <koshtisagar7778@gmail.com>
2025-04-04 16:26:26 +0530

    fix issue

--- src/components/services/ServiceOne.js
+++ src/components/services/ServiceOne.js
@@ -73,7 +73,9 @@ function ServiceOne() {
                     {/* More Solutions Button */}
                     <div className="row mt-4">
                         <div className="col-lg-12 text-center">
-                            <a href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ39f071tVvYbKUAnVaFk3HQG4uAEoL-CjHGygO7nfiRCA6lwLnehAPwYCHMdH5s5S7f8-WzjLse" className="rts-btn btn-primary">
+                            <a
+                                target="_blank"
+                                href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ39f071tVvYbKUAnVaFk3HQG4uAEoL-CjHGygO7nfiRCA6lwLnehAPwYCHMdH5s5S7f8-WzjLse" className="rts-btn btn-primary">
                                 More Solutions
                                 <ReactSVG src="assets/images/service/icons/13.svg" alt="arrow" />
                             </a>
