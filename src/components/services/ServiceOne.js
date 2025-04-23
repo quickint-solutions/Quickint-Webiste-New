265036d987f84552bdedb1020199398f8fce32e8
koshti sagar <koshtisagar7778@gmail.com>
2025-04-23 14:57:53 +0530

    More solutions should be redirect to all services page

--- src/components/services/ServiceOne.js
+++ src/components/services/ServiceOne.js
@@ -74,8 +74,7 @@ function ServiceOne() {
                     <div className="row mt-4">
                         <div className="col-lg-12 text-center">
                             <a
-                                target="_blank"
-                                href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ39f071tVvYbKUAnVaFk3HQG4uAEoL-CjHGygO7nfiRCA6lwLnehAPwYCHMdH5s5S7f8-WzjLse" className="rts-btn btn-primary">
+                                href="/servicepage" className="rts-btn btn-primary">
                                 More Solutions
                                 <ReactSVG src="assets/images/service/icons/13.svg" alt="arrow" />
                             </a>
